96c1a20e2557049cfe1859d3304821deafc55824
John Segner <john.segner@gmail.com>
2020-04-20 22:31:07 -0400

    motion bug 2nd try

--- motion.ino
+++ motion.ino
@@ -66,7 +66,10 @@ void runPreset(int num){
     springEn = true;
   }
   //currentPreset = num;
-  num = num + (batterHand*9);
+  //num = num + (batterHand*9);
+  if(!batterHand){
+    num=num+9;
+  }
   pitchSet = map(constrain(presets[num][0],0,100),0,100,PITCHMIN,PITCHMAX);
   yawSet = map(constrain(presets[num][1],0,100),0,100,YAWMIN,YAWMAX);
   springSet = map(constrain(presets[num][2],0,100),0,100,SPRINGMIN,SPRINGMAX);
