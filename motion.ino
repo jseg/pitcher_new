931ef62566d4b1b3dcc695cc569c672e087a8b43
John Segner <john.segner@gmail.com>
2020-04-20 22:21:04 -0400

    revert

--- motion.ino
+++ motion.ino
@@ -66,7 +66,7 @@ void runPreset(int num){
     springEn = true;
   }
   //currentPreset = num;
-  num = (num - (batterHand*9))+9;
+  num = num + (batterHand*9);
   pitchSet = map(constrain(presets[num][0],0,100),0,100,PITCHMIN,PITCHMAX);
   yawSet = map(constrain(presets[num][1],0,100),0,100,YAWMIN,YAWMAX);
   springSet = map(constrain(presets[num][2],0,100),0,100,SPRINGMIN,SPRINGMAX);
